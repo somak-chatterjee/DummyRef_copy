4e4f1e859757c4ac7d2d956ae477f13cf8ece9eb
pyref <ppyref@gmail.com>
2021-08-03 20:55:27 +0200

    Push Down Method Ref

--- example.py
+++ example.py
@@ -2,6 +2,3 @@ class example_class:
     def methodA(self):
         print(0)
         total = 1 + 2 + 3
-
-    def methodB(self):
-        obj = a + c
--- example_1.py
+++ example_1.py
@@ -2,5 +2,9 @@ from example import *
 
 
 class example_class_child(example_class):
+
     def methodC(self):
         print("Hello World")
+
+    def methodB(self):
+        obj = a + func(c)
