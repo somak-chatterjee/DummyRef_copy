87d6c845c46f393d8b23ae788efbe1ee55a856ac
pyref <ppyref@gmail.com>
2021-08-03 20:00:59 +0200

    Extract Method Ref

--- example.py
+++ example.py
@@ -1,4 +1,8 @@
 class example:
     def methodA(self):
-        obj = obj1.func1().func2().func3()
+        self.methodB()
+
+    def methodB(self):
+        ext_obj = obj1.func1()
+        obj = ext_obj.func2().func3()
         print("End")
