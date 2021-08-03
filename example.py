68c9cf8e43e62a3bbb1043028ce93da8f23eb2f2
pyref <ppyref@gmail.com>
2021-08-03 20:58:57 +0200

    Extract & Rename Method Ref

--- example.py
+++ example.py
@@ -1,6 +1,13 @@
+"""Rename & Extract Method
+"""
+
+
 class example_class:
-    def methodB(self):
+    def methodC(self):
         print(0)
         if True:
             x = 1 + 2 + 3
+        self.methodD()
+
+    def methodD(self):
         print("Extract_Rename")
